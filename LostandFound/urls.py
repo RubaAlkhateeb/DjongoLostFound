aed1f18e457473b278b015461ec01eea1ad85f19
Ruba Alkhateeb <52524284+RubaAlkhateeb@users.noreply.github.com>
2020-09-23 21:40:24 +0300

    add viewitem and add page

--- LostandFound/urls.py
+++ LostandFound/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-
-from . import views
+#from . import views
+from .views import IndexView, AddView, ItemView
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('', views.Add, name='Add'),
+    # path('', views.index, name='index'),
+    # path('Add/', views.Add, name='Add'),
+    path('', IndexView.as_view(), name="index"),
+    path('Add/', AddView.as_view(), name='Add'),
+    path('viewitem/<int:pk>', ItemView.as_view(), name='viewitem'),
 ]
--- LostandFound/views.py
+++ LostandFound/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from .models import Post
 
+# def index(request):
+#     #template = loader.get_template('LostandFound/index.html')
+#     return render(request, 'index.html',{})
 
-def index(request):
-    #template = loader.get_template('LostandFound/index.html')
-    return render(request, 'index.html',{})
+# def Add(request):
+#     #template = loader.get_template('LostandFound/index.html')
+#     return render(request, 'Add.html',{})
 
-def Add(request):
-    #template = loader.get_template('LostandFound/index.html')
-    return render(request, 'Add.html',{})
+class IndexView(ListView):
+    model = Post
+    template_name = 'index.html'
 
+class AddView(ListView):
+    model = Post
+    template_name = 'Add.html'
+
+class ItemView(DetailView):
+    model = Post
+    template_name = 'viewitem.html'
